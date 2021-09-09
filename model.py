8c9f7a771ddfaf4d6d051882e36c25cc1d2c1bc6
atreyu595 <jasperc09@yahoo.com>
2021-09-09 18:11:12 +1000

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+#Import necessary libraries
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn
 
+#THIS IS THE DIRECTORY OF YOUR DATA IN YOUR LOCAL FILE
 data = pd.read_csv(r'C:\Users\Atrey\Documents\USA_Housing.csv')
 
 
@@ -16,10 +17,10 @@ data = pd.read_csv(r'C:\Users\Atrey\Documents\USA_Housing.csv')
 x = data.loc[:,'Avg. Area Income':'Area Population']
 x = x.astype(int)
 
-
 y = data.loc[:,'Price']
 y = y.astype(int)
 
+#Split data
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 regressor = LinearRegression()
