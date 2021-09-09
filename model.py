3b9b1a9d6751860cb659fec4f64e764635529a41
atreyu595 <jasperc09@yahoo.com>
2021-09-09 18:12:16 +1000

    Update model.py

--- model.py
+++ model.py
@@ -23,12 +23,11 @@ y = y.astype(int)
 #Split data
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
+#INitialize regressor for regression analysis
 regressor = LinearRegression()
 
+#Fit model 
 regressor.fit(X_train, y_train)
 
-regressor.score(X_train, y_train)
-
-regressor.score(X_test, y_test)
-
+#Save model for application
 pickle.dump(regressor, open('model.pkl', 'wb'))
